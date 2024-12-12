3bbe81eecebbf937a393aba4c37d0df38e88ba20
vinceTheProgrammer <vincetheprogrammer@gmail.com>
2024-12-12 00:08:31 -0600

    maybe broken

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,15 @@
 use gstreamer as gst;
+use std::ptr;
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use windows::Win32::System::LibraryLoader::SetDllDirectoryW;
+
+fn set_dll_directory(path: &str) {
+    let wide: Vec<u16> = OsStr::new(path).encode_wide().chain(Some(0)).collect();
+    unsafe {
+        SetDllDirectoryW(wide.as_ptr());
+    }
+}
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -19,6 +30,9 @@ fn get_gstreamer_version() -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // Set the DLL directory to "dlls" (relative to the executable)
+    set_dll_directory("dlls");
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet, get_gstreamer_version])
