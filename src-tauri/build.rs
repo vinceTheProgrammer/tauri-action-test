bd92f6e08b62d7cf011582fa4361a9a5e252100b
vinceTheProgrammer <vincetheprogrammer@gmail.com>
2024-12-12 15:32:52 -0600

    this is the one

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs::{self, copy};
+use std::path::Path;
+
 fn main() {
+    
+    if cfg!(windows) {
+        let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let lib_dir = Path::new(&dir).join("lib").join("windows").join("64");
+
+        // Iterate over all .dll files in the lib_dir and copy them to the root directory
+        for entry in fs::read_dir(&lib_dir).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.extension().map_or(false, |ext| ext == "dll") {
+                let file_name = path.file_name().unwrap();
+                let dest_path = Path::new(&dir).join(file_name);
+                println!("cargo:rerun-if-changed={}", path.display()); // Watch for changes
+                copy(&path, &dest_path).unwrap();
+            }
+        }
+
+        // Add library directory to the linker search path
+        println!("cargo:rustc-link-search=native={}", lib_dir.display());
+    }
+
     tauri_build::build()
 }
